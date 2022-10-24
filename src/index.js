3495fc83ec3d4f296bbcf33ea1aa51fa92eee71b
Epricecheque <Eprice2139@gmail.com>
2022-10-24 00:24:20 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -8,6 +8,31 @@ import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Rinkeby;
+
+  
+
+
+// Wrap your app with the thirdweb provider
+ReactDOM.render(
+  <React.StrictMode>
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+); */
+
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+// Import thirdweb provider and Goerli ChainId
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Goerli;
 
 // Wrap your app with the thirdweb provider
 ReactDOM.render(
